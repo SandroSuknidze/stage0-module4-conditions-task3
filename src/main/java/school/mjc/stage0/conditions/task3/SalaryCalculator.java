1613244b882134ebcbbdbfb59d1eb11a66401eda
Sandro Suqnidze <sandrosuqnidze2@gmail.com>
2023-09-10 11:20:35 +0400

    if + logical (changedTwo)

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -6,7 +6,7 @@ public class SalaryCalculator {
             System.out.println((double) salary - salary * 15 / 100);
         } else if (10000 < salary && salary <= 20000) {
             System.out.println((double) salary - salary * 18 / 100);
-        } else if (20000 > salary) {
+        } else if (20000 < salary) {
             System.out.println((double) salary - salary * 28 / 100);
         } else {
             System.out.println("wrong input!");
